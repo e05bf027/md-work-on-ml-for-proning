2f89552b075c6fbb675a85c1660af37dc28275fe
David M Hannon <62836608+e05bf027@users.noreply.github.com>
2023-03-30 12:43:41 +0100

    update dataset
    updates dataset from before manipulations are made using the script 'change_position_values.R'

--- data/change_position_values.R
+++ data/change_position_values.R
@@ -2,3 +2,5 @@ pre_post_raw['position'][pre_post_raw['position'] == 'supine'] <- 'supine_at_adm
 pre_post_raw['position'][pre_post_raw['position'] == 'supine_pre'] <- 'supine_pre_proning'
 pre_post_raw['position'][pre_post_raw['position'] == 'prone'] <- 'prone_end_of_session'
 pre_post_raw['position'][pre_post_raw['position'] == 'supine_post'] <- 'supine_post_proning'
+
+save(pre_post_raw, file = '~/Documents/02. Medicine/04. MD/Strand 03 - ML/md-work-on-ml-for-proning/data/dataset_before_manipulation.Rda')
