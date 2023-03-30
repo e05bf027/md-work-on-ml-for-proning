6acf59b822ae02cf0ceeb1c6e05e32621e3566a5
David M Hannon <62836608+e05bf027@users.noreply.github.com>
2023-03-30 22:19:25 +0100

    Update determination_of_retained_response.R
    addition of step to return logical value for whether patient responded or not

--- retained_changes/determination_of_retained_response.R
+++ retained_changes/determination_of_retained_response.R
@@ -3,3 +3,15 @@
 # prone positioning by analyzing if their PF ratio after returning to the 
 # supine position is GREATER THAN the Pf ratio before proning.
 # -----------------------------------------------------------------------
+
+# setup
+set.seed(3.14)
+library(tidyverse)
+load(file = 'data/pre_post_changes.Rda')
+
+# add if patient responded
+pre_post_changes$pf_responder <- if_else(condition = (pre_post_changes$pf_ratio_supine_post - pre_post_changes$pf_ratio_supine) > 0,
+                                         true = T, 
+                                         false = F)
+
+
