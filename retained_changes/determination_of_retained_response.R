1d59619e1d1dbe12cd64df5d541d086e27dd7cbf
David M Hannon <62836608+e05bf027@users.noreply.github.com>
2023-03-30 21:29:06 +0100

    Create determination_of_retained_response.R

---
+++ retained_changes/determination_of_retained_response.R
@@ -0,0 +1,5 @@
+# determination_of_retained_response.R
+# this script decides if a patient has 'responded' to being placed in the
+# prone positioning by analyzing if their PF ratio after returning to the 
+# supine position is GREATER THAN the Pf ratio before proning.
+# -----------------------------------------------------------------------
