08065fa3c17b87e1bae25e7f80453a4a39866b25
David M Hannon <62836608+e05bf027@users.noreply.github.com>
2023-07-13 22:43:05 +0100

    update naive bayes approach

---
+++ naive_bayes/breaks_fold_1.R
@@ -0,0 +1,17 @@
+# 2023-06-23 13:39:47 
+options(scorecard.bin_close_right = FALSE) 
+list(
+ age_years=c("44", "62", "68", "74"), 
+ pf_ratio_supine=c("8.5%,%missing", "10", "11", "12.5"), 
+ pfr_change_absolute=c("9%,%missing", "17", "21"), 
+ aa_gradient_paco2_supine=c("34%,%missing", "38", "44", "58", "66", "76"), 
+ aa_paco2_change_absolute=c("-50%,%missing", "-44", "-36", "-20", "-18", "-2"), 
+ urea_supine=c("7.5%,%missing", "9.5", "20.5"), 
+ pcre_supine=c("55%,%missing", "85", "90", "100", "110", "135"), 
+ total_haemoglobin_supine=c("8.6", "9.2", "9.6", "10", "12"), 
+ platelet_count_supine=c("100%,%missing", "150", "200", "380"), 
+ albumin_supine=c("28%,%missing", "32", "34"), 
+ white_cell_count_supine=c("6.5%,%missing", "13", "22.5"), 
+ c_reactive_protein_supine=c("20%,%missing", "60", "80", "120", "200", "290"), 
+ lactate_abg_supine=c("0.6%,%missing", "1", "1.2", "1.6", "1.8", "2.8")
+ )
